d6e4b6c5f469b33541918f550e6748ea92bb3a01
tuan <tuan.tran.mail@gmail.com>
2015-02-24 14:12:55 +0100

    add controller post

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,7 @@ return [
         'Application',
         'DoctrineModule',
         'DoctrineORMModule',
+        'Administration',
     ],
 
     // These are various options for the listeners attached to the ModuleManager
